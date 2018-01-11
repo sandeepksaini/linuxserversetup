85aecc0e6d24dbe52d16bda5fe66dfc65e0f7daf
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-11 13:18:36 +0200

    Update installpackage.sh
    Added epel

--- installpackage.sh
+++ installpackage.sh
@@ -2,7 +2,7 @@
 #Purpose: To install the preliminary package for the setup of the host server
 
 #Install using yum package installer
-for package_list in bash-completion fish screen; 
+for package_list in bash-completion fish screen epel; 
 do 
  yum install ${package_list} -y
 done
