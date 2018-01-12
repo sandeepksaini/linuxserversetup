1fcf6e8b30563d194cdbcd3cbd356a334677fe39
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-12 21:12:18 +0200

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -5,6 +5,7 @@
 
 function rdp_setup() 
 {
+yum install firewalld -y
 yum -y install epel-release && rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 yum install xrdp tigervnc-server
 systemctl start firewalld
