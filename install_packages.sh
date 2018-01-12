96c7f75a4dd1315431def805711cb8f93d3a7633
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-12 21:16:26 +0200

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -9,7 +9,7 @@ yum install firewalld -y
 yum -y install epel-release && rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 yum install xrdp tigervnc-server
 systemctl start firewalld
-filrewall-cmd --permanent --zone=public --add-port=3389/tcp
+firewall-cmd --permanent --zone=public --add-port=3389/tcp
 firewall-cmd --reload
 chcon --type=bin_t /usr/sbin/xrdp
 chcon --type=bin_t /usr/sbin/xrdp-sesman 
