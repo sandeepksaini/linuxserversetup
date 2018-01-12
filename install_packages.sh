62b1efa0a071fc863b51760232c48c0eca0d6968
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-12 20:40:33 +0200

    Update and rename install_rdp.sh to install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Purpose: The purpose of this shell script is to install and setup a RDP server and tiger vnc on CentOS machine.
+# Purpose: 
+# 1) The purpose of this shell script is to install and setup a RDP server and tiger vnc on CentOS machine.
+# 2) Install git and configure it
 
 function rdp_setup() 
 {
@@ -14,4 +16,17 @@ systemctl start xrdp.service
 systemctl enable xrdp.service
 }
 
+function git_install()
+{
+if [ `yum list git|grep Installed|wc -l` -eq 0 ];
+then
+  yum group-install "Development Tools"
+  yum install git -y
+  git config --global user.name "SandeepKumar"
+  git config --global user.email "sandeeprhct@gmail.com"
+  
+fi
+}
+
 rdp_setup
+git_install
