42100095a2f3f5efd41bdc90e0b64d629534f522
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-15 11:28:21 +0200

    Delete installpackage.sh

--- installpackage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Purpose: To install the preliminary package for the setup of the host server
-
-#Install using yum package installer
-for package_list in bash-completion fish screen epel; 
-do 
- yum install ${package_list} -y
-done
